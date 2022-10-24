b7648693e9db1240b43f2c9128dd47b9399bb10e
nevvrmbk <99172207+nevvrmbk@users.noreply.github.com>
2022-10-24 15:24:13 +0300

    KV Bucket.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,26 @@
 export const load: any = async ({ cookies, fetch, getClientAddress, platform, request }: any) => {
-    console.log(cookies);
-    console.log(fetch);
-    console.log(getClientAddress());
-    console.log(platform);
-    console.log(request);
+	console.log(cookies);
+	console.log(fetch);
+	console.log(getClientAddress());
+	console.log(platform);
+	console.log(request);
 
-    if (platform.env) {
-        return {
-            message: "Platform env is avaliable."
-        };
-    }
+	if (platform.env) {
+		try {
+			await platform.env.Bucket.put('one', 'Jessica Alba');
+			await platform.env.Bucket.put('two', 'Jamie Alexander');
+			await platform.env.Bucket.put('three', 'Jennifer Lawrence');
+			await platform.env.Bucket.put('four', 'Hailee Steinfeld');
+			await platform.env.Bucket.put('five', 'Amanda Setfried');
+		} catch (err) {
+			console.log(err);
+		}
+		return {
+			message: 'Platform env is avaliable.'
+		};
+	}
 
-    return {
-        message: "Platform env is not available."
-    };
+	return {
+		message: 'Platform env is not available.'
+	};
 };
